a32da6dc4fb51df03f9c30886422886913d4beaa
Joshua Gugun Siagian <joshua.siagian@binus.ac.id>
2024-11-12 19:46:35 +0000

    prepare

--- src/datasets.py
+++ src/datasets.py
@@ -20,4 +20,19 @@ class IndoSum:
             type (str): The split to convert to a DataFrame. Options are "train", "validation", and "test".
         """
         return pd.DataFrame(self.ds[type])
+    
+    # update the self.ds value
+    def update(self, new_ds):
+        """
+        Update the dataset.
+        
+        Args:
+            new_ds (datasets.Dataset): The new dataset to update to.
+            
+        Returns:
+            datasets.Dataset: The updated dataset.
+        """
+        self.ds = new_ds
+        
+        return self.ds
 
