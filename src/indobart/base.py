d4e033f14ffb9aeac203da6716de3d665b575ccd
Joshua Gugun Siagian <joshua.siagian@binus.ac.id>
2024-11-14 18:13:08 +0000

    prepare DP

--- src/indobart/base.py
+++ src/indobart/base.py
@@ -1,6 +1,5 @@
-from transformers import AutoModelForSeq2SeqLM
+from transformers import AutoModelForSeq2SeqLM, BartConfig, AutoConfig
 from .tokenizer import IndoNLGTokenizer
-
 
 def get_model():
     return AutoModelForSeq2SeqLM.from_pretrained("indobenchmark/indobart-v2")
@@ -8,3 +7,7 @@ def get_model():
 
 def get_tokenizer():
     return IndoNLGTokenizer.from_pretrained("indobenchmark/indobart-v2")
+
+def get_config():
+    # return AutoConfig.from_pretrained("indobenchmark/indobart-v2")
+    return BartConfig.from_pretrained("indobenchmark/indobart-v2")
